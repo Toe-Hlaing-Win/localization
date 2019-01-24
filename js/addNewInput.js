9d4626a6661429b9d975b5de6a2e2bd3dec17fd8
Toe Hlaing Win <sokkerthwinn@gmail.com>
2019-01-24 10:23:29 +0630

    chage text to textarea

--- js/addNewInput.js
+++ js/addNewInput.js
@@ -6,34 +6,42 @@ window.onload = function() {
             var keyName = document.getElementById('key-name').value;
             console.log(keyName);
             // for Key
-            var text = document.createElement('input');
+            var text = document.createElement('textarea');
             text.type = 'text';
-            text.id =  "mmunicode-" + keyName;
-            text.size = "40";
+            text.id =  keyName;
+            text.rows = 2;
+            text.cols = 39;
+            text.style.margin = "2px";
             text.value = keyName;
             document.getElementById('keys').appendChild(text);
 
             // for Eng
-            var text = document.createElement('input');
+            var text = document.createElement('textarea');
             text.type = 'text';
             text.id =  "eng-" + keyName;
-            text.size = "40";
+            text.rows = 2;
+            text.cols = 39;
+            text.style.margin = "2px";
             text.value = keyName;
             document.getElementById('val').appendChild(text);
 
             // for Unicode
-            var text = document.createElement('input');
+            var text = document.createElement('textarea');
             text.type = 'text';
             text.id =  "mmunicode-" + keyName;
-            text.size = "40";
+            text.rows = 2;
+            text.cols = 39;
+            text.style.margin = "2px";
             text.value = "";
             document.getElementById('valUnicode').appendChild(text);
 
             // for Zawgyi
-            var text = document.createElement('input');
+            var text = document.createElement('textarea');
             text.type = 'text';
             text.id =  "mmzawgyi-" + keyName;
-            text.size = "40";
+            text.rows = 2;
+            text.cols = 39;
+            text.style.margin = "2px";
             text.value = "";
             document.getElementById('valZawgyi').appendChild(text);
 
